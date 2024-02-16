40d5680dffcefdcc2bc99af8d0895f23cccea602
Rishap041 <rishapkumar94@gmail.com>
2024-02-16 20:32:46 +0530

    User login API.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
 
 app.listen(7000, () => {
     console.log('server running on localhost:7000');
