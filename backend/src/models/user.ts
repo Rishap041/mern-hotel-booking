3f52fadc3978b0f8ef8ea0210065465f0eb850a5
Rishap041 <rishapkumar94@gmail.com>
2024-02-16 14:31:18 +0530

    User registration API.

---
+++ backend/src/models/user.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export type UserType = {
+    _id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+})
+
+//It is a Mongoose schema pre-save hook that hashes the user's password before it is saved to the database.
+userSchema.pre("save", async function (next) {
+    //This line checks to see if the user's password has been modified.
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
+})
+
+const User = mongoose.model<UserType>("User", userSchema);
+
+export default User;
